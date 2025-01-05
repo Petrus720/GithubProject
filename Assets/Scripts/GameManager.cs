f579ece9fddeb150553ec65891c168e45ee6ebee
Petrus720 <rueda.pedro004@gmail.com>
2025-01-05 14:08:04 +0100

    GameManager Script
    This script is the main script and controls every stage of the game.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Singleton: to use methods from this class I don't need to get a component
+    public static GameManager Instance; //Capitalize cause this is a static reference to my object
+
+    private int playerPoints = 0;
+    public int PlayerPoints { get => playerPoints; set => playerPoints = value; }
+
+    [SerializeField] public int lifePoints = 5;
+
+    private void Awake()
     {
-        
+        //Singleton, reference Instance to this object instance
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            //assign the static instance to the object
+            Instance = this;
+            //maintain this instance to the next scene
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+    private void Start()
+    {
+        PlayerPrefs.SetInt("health", lifePoints);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
